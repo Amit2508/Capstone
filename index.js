33abd5b5c2cf10113b0d78d857939bb6dedb72c6
rrsethi <rrs.niit@gmail.com>
2023-03-02 18:02:19 +0530

    resolving errors

--- index.js
+++ index.js
@@ -1,187 +1,119 @@
 var display = document.getElementById("screen");
 var show = "";
-var check = true;
-var op1 = "";
-var op2 = "";
-var opr = "";
-
 function calc() {
-  var value = this.getAttribute("data-value");
-
-  //for clear
-  if (value === "AC") {
-    check = true;
-    show = "";
-    op1 = "";
-    op2 = "";
-    opr = "";
-    display.innerText = show;
-  }
-
-  // logic for simple operator
-  else if (
-    value === "+" ||
-    value === "-" ||
-    value === "*" ||
-    value === "/" ||
-    value === "%"
+  value = this.getAttribute("data-value");
+  calculate(value);
+}
+document.addEventListener("keydown", function (event) {
+  if (
+    [
+      "+",
+      "-",
+      "*",
+      "/",
+      "=",
+      ".",
+      "1",
+      "2",
+      "3",
+      "4",
+      "5",
+      "6",
+      "7",
+      "8",
+      "9",
+      "0",
+    ].includes(event.key)
   ) {
-    if (show === "" || opr !== "") {
-      display.innerText = "Error!";
-    } else {
-      opr = value;
-      check = true;
-      op1 = show;
-      show = "";
-      display.innerText = show;
-    }
+    value = event.key;
+    calculate(value);
   }
+  if (event.key == "Escape") {
+    value = "AC";
+    calculate(value);
+  }
+  if (event.key == "Backspace") {
+    value = "⬅";
+    calculate(value);
+  }
+});
 
-  // final evaluation
-  else if (value === "=") {
-    if (show === "" || (opr === "/" && show === "0")) {
-      display.innerText = "Division Error!";
-      show = "";
-    } else if (opr === "-" && show[0] === "-") {
-      opr = "+";
-      op2 = show.slice(1);
-      show = eval(op1 + opr + op2);
-      if (Number.isInteger(show) === false) {
-        show = show.toFixed(3);
-      }
-      display.innerText = show;
-      opr = "";
-    } else {
-      check = true;
-      op2 = show;
-      if (opr === "%") {
-        show = op1 % op2;
+function calculate(value) {
+  if (value == "AC") {
+    show = "";
+    display.innerText = show;
+  } else if (value == "=") {
+    if (["+", "-", "/", "*", "."].includes(show.charAt(show.length - 1))) {
+      show = show.slice(0, -1);
+    }
+    ans = eval(show);
+    display.innerText = ans;
+    show = "";
+  } else {
+    if (["+", "-", "/", "*"].includes(value)) {
+      if (show == "") {
+        display.innerText = "Error";
+      } else if (
+        ["+", "-", "/", "*", "."].includes(show.charAt(show.length - 1))
+      ) {
+        show = show.slice(0, -1);
+        show += value;
+        display.innerText = show;
       } else {
-        show = eval(op1 + opr + op2);
+        show += value;
+        display.innerText = show;
       }
-      if (Number.isInteger(show) === false) {
-        show = show.toFixed(3);
+    } else if (value == "⬅") {
+      if (show != "") {
+        show = show.slice(0, -1);
+        display.innerText = show;
       }
-      display.innerText = show;
-      opr = "";
-    }
-  }
-
-  // for decimal
-  else if (value === ".") {
-    if (check === true) {
-      check = false;
+    } else if (value == "+/-") {
+      if (show == "") {
+        show += "-";
+        console.log(show);
+        display.innerText = show;
+      } else if (show.charAt(show.length - 1) == "-") {
+        show = show.slice(0, -1);
+        display.innerText = show;
+      } else if (show.charAt(show.length - 1) == "+") {
+        show = show.slice(0, -1);
+        show += "-";
+        display.innerText = show;
+      } else if (["/", "*"].includes(show.charAt(show.length - 1))) {
+        show += "-";
+        display.innerText = show;
+      } else {
+        lcA = show.lastIndexOf("+");
+        lcS = show.lastIndexOf("-");
+        lcM = show.lastIndexOf("*");
+        lcD = show.lastIndexOf("/");
+        mlc = Math.max.apply(null, [lcA, lcS, lcM, lcD]);
+        console.log(mlc);
+        if (mlc == -1) {
+          show = "-" + show;
+        } else if (mlc == 0) {
+          show = show.slice(1);
+        } else if (show.charAt(mlc) == "+") {
+          show = show.substring(0, mlc) + "-" + show.substring(mlc + 1);
+        } else if (show.charAt(mlc) == "-") {
+          if (["*", "/"].includes(show.charAt(mlc - 1))) {
+            show = show.substring(0, mlc) + show.substring(mlc + 1);
+          } else {
+            show = show.substring(0, mlc) + "+" + show.substring(mlc + 1);
+          }
+        } else {
+          show = show.substring(0, mlc + 1) + "-" + show.substring(mlc + 1);
+        }
+        display.innerText = show;
+      }
+    } else {
       show += value;
       display.innerText = show;
-    } else {
-      display.innerText = "Error!";
     }
-  }
-
-  //For changing positive no.to -ve or vice versa(+/-)
-  else if (value === "+/-") {
-    if (show[0] === "-") {
-      show = show.slice(1);
-      display.innerText = show;
-    } else {
-      show = "-" + show;
-      display.innerText = show;
-    }
-  }
-  // for all numeric 0 to 9
-  else {
-    show += value;
-    display.innerText = show;
   }
 }
 var btn_bucket = document.getElementsByClassName("btn");
 for (var i = 0; i < btn_bucket.length; i++) {
   btn_bucket[i].addEventListener("click", calc);
 }
-
-
-document.addEventListener("keydown", function (event) {
-  // console.log("Key pressed: " + event.key);
-  if (event.key in [1, 2, 3, 4, 5, 6, 7, 8, 9, 0]) {
-    show += event.key;
-    display.innerText = show;
-  }
-  if (event.key == "Backspace") {
-    if (show.length != 0) {
-      show = show.slice(0, -1);
-      display.innerText = show;
-    }
-  }
-
-  if (event.key == "Escape") {
-    check = true;
-    show = "";
-    op1 = "";
-    op2 = "";
-    opr = "";
-    display.innerText = show;
-  }
-  if (["+", "-", "*", "/", "%"].includes(event.key)) {
-    // console.log(event.key, "rr")
-    if (show === "" || opr !== "") {
-      display.innerText = "Error!";
-    } else {
-      opr = event.key;
-      check = true;
-      op1 = show;
-      show = "";
-      display.innerText = show;
-    }
-  }
-  if (event.key == "^") {
-    // console.log(event.key, "rr")
-    if (show === "" || opr !== "") {
-      display.innerText = "Error!";
-    } else {
-      opr = "**";
-      check = true;
-      op1 = show;
-      show = "";
-      display.innerText = show;
-    }
-  }
-  if (["=", "Enter"].includes(event.key)) {
-    if (show === "" || (opr === "/" && show === "0")) {
-      display.innerText = "Division Error!";
-      show = "";
-    } else if (opr === "-" && show[0] === "-") {
-      opr = "+";
-      op2 = show.slice(1);
-      // console.log(op1 + opr + op2)
-      show = eval(op1 + opr + op2);
-      if (Number.isInteger(show) === false) {
-        show = show.toFixed(3);
-      }
-      display.innerText = show;
-      opr = "";
-    } else {
-      check = true;
-      op2 = show;
-      if (opr === "%") {
-        show = op1 % op2;
-      } else {
-        show = eval(op1 + opr + op2);
-      }
-      if (Number.isInteger(show) === false) {
-        show = show.toFixed(3);
-      }
-      display.innerText = show;
-      opr = "";
-    }
-  }
-  if (event.key == ".") {
-    if (check === true) {
-      check = false;
-      show += event.key;
-      display.innerText = show;
-    } else {
-      display.innerText = "Error!";
-    }
-  }
-
-});
