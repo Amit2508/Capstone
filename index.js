4ac5c69f32e994735796e6077158080ffe33ee4b
BhawnaGarg-2508 <bhawna.garg20@st.niituniversity.in>
2023-03-02 17:38:52 +0530

    errors resolved properly

--- index.js
+++ index.js
@@ -1,167 +1,118 @@
 var display = document.getElementById("screen");
 var show = "";
-var check = true;
-var op1 = "";
-var op2 = "";
-var opr = "";
-var restart = false;
-var value;
 function calc() {
   value = this.getAttribute("data-value");
   calculate(value);
 }
 document.addEventListener("keydown", function (event) {
-  // console.log("Key pressed: " + event.key);
-  if (event.key in [1, 2, 3, 4, 5, 6, 7, 8, 9, 0]) {
+  if (
+    [
+      "+",
+      "-",
+      "*",
+      "/",
+      "=",
+      ".",
+      "1",
+      "2",
+      "3",
+      "4",
+      "5",
+      "6",
+      "7",
+      "8",
+      "9",
+      "0",
+    ].includes(event.key)
+  ) {
     value = event.key;
     calculate(value);
-    // display.innerText = show;
-  }
-  if (event.key == "Backspace") {
-    if (show.length != 0) {
-      show = show.slice(0, -1);
-      display.innerText = show;
-    }
   }
   if (event.key == "Escape") {
     value = "AC";
     calculate(value);
   }
-  if (event.key == "Enter") {
-    value = "=";
-    calculate(value);
-  }
-  if (["+", "-", "*", "/", "%", "=", "."].includes(event.key)) {
-    // console.log(event.key, "rr")
-    value = event.key;
+  if (event.key == "Backspace") {
+    value = "⬅";
     calculate(value);
   }
 });
 
-function res() {
-  if (show === "") {
-    // display.innerText = "";
-    rVal = ""
-  }
-  else if (opr === "/" && show === "0") {
-    rVal = "Division Error!";
-    opr = "";
-    // rVal = "";
-  } else if (opr === "-" && show[0] === "-") {
-    opr = "+";
-    op2 = show.slice(1);
-    rVal = eval(op1 + opr + op2);
-    if (Number.isInteger(rVal) === false) {
-      rVal = rVal.toFixed(3);
-    }
-    // display.innerText = show;
-    opr = "";
-    op1 = ""
-    op2 = ""
-  } else {
-    check = true;
-    op2 = show;
-    if (opr === "%") {
-      rVal = op1 % op2;
-    } else {
-      rVal = eval(op1 + opr + op2);
-    }
-    if (Number.isInteger(rVal) === false) {
-      rVal = rVal.toFixed(3);
-    }
-    // display.innerText = show;
-    opr = "";
-    op1 = ""
-    op2 = ""
-  }
-  return rVal
-}
 function calculate(value) {
-  //for clear
-  console.log(restart)
-  if (restart == true) {
-    check = true;
+  if (value == "AC") {
     show = "";
-    op1 = "";
-    op2 = "";
-    opr = "";
     display.innerText = show;
-    restart = false;
-  }
-  if (value === "AC") {
-    check = true;
+  } else if (value == "=") {
+    if (["+", "-", "/", "*", "."].includes(show.charAt(show.length - 1))) {
+      show = show.slice(0, -1);
+    }
+    ans = eval(show);
+    display.innerText = ans;
     show = "";
-    op1 = "";
-    op2 = "";
-    opr = "";
-    display.innerText = show;
-  }
-
-  // logic for simple operator
-  else if (
-    value === "+" ||
-    value === "-" ||
-    value === "*" ||
-    value === "/" ||
-    value === "%"
-  ) {
-    console.log(show)
-    if (show === "") {
-      display.innerText = "Error!";
-    } else if (opr !== "") {
-      if (op1.isInteger) {
-        show = res()
-        op1 = show
+  } else {
+    if (["+", "-", "/", "*"].includes(value)) {
+      if (show == "") {
+        display.innerText = "Error";
+      } else if (
+        ["+", "-", "/", "*", "."].includes(show.charAt(show.length - 1))
+      ) {
+        show = show.slice(0, -1);
+        show += value;
+        display.innerText = show;
+      } else {
+        show += value;
         display.innerText = show;
       }
-
-    }
-    else {
-      opr = value;
-      check = true;
-      op1 = show;
-      show = "";
-      display.innerText = show;
-    }
-  }
-
-  // final evaluation
-  else if (value === "=") {
-    restart = true;
-    show = res();
-    display.innerText = show
-
-    // restart = true;
-  }
-
-  // for decimal
-  else if (value === ".") {
-    if (check === true) {
-      check = false;
+    } else if (value == "⬅") {
+      if (show != "") {
+        show = show.slice(0, -1);
+        display.innerText = show;
+      }
+    } else if (value == "+/-") {
+      if (show == "") {
+        show += "-";
+        console.log(show);
+        display.innerText = show;
+      } else if (show.charAt(show.length - 1) == "-") {
+        show = show.slice(0, -1);
+        display.innerText = show;
+      } else if (show.charAt(show.length - 1) == "+") {
+        show = show.slice(0, -1);
+        show += "-";
+        display.innerText = show;
+      } else if (["/", "*"].includes(show.charAt(show.length - 1))) {
+        show += "-";
+        display.innerText = show;
+      } else {
+        lcA = show.lastIndexOf("+");
+        lcS = show.lastIndexOf("-");
+        lcM = show.lastIndexOf("*");
+        lcD = show.lastIndexOf("/");
+        mlc = Math.max.apply(null, [lcA, lcS, lcM, lcD]);
+        console.log(mlc);
+        if (mlc == -1) {
+          show = "-" + show;
+        } else if (mlc == 0) {
+          show = show.slice(1);
+        } else if (show.charAt(mlc) == "+") {
+          show = show.substring(0, mlc) + "-" + show.substring(mlc + 1);
+        } else if (show.charAt(mlc) == "-") {
+          if (["*", "/"].includes(show.charAt(mlc - 1))) {
+            show = show.substring(0, mlc) + show.substring(mlc + 1);
+          } else {
+            show = show.substring(0, mlc) + "+" + show.substring(mlc + 1);
+          }
+        } else {
+          show = show.substring(0, mlc + 1) + "-" + show.substring(mlc + 1);
+        }
+        display.innerText = show;
+      }
+    } else {
       show += value;
       display.innerText = show;
-    } else {
-      display.innerText = "Error!";
     }
-  }
-
-  //For changing positive no.to -ve or vice versa(+/-)
-  else if (value === "+/-") {
-    if (show[0] === "-") {
-      show = show.slice(1);
-      display.innerText = show;
-    } else {
-      show = "-" + show;
-      display.innerText = show;
-    }
-  }
-  // for all numeric 0 to 9
-  else {
-    show += value;
-    display.innerText = show;
   }
 }
-// }
 var btn_bucket = document.getElementsByClassName("btn");
 for (var i = 0; i < btn_bucket.length; i++) {
   btn_bucket[i].addEventListener("click", calc);
